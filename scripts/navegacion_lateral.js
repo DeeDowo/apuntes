c4634aa7b77011ceaef4581b2d76ed46269e0d01
Dowo <jo.jo.zen501@gmail.com>
2025-06-26 18:08:44 -0600

    revisión diaria

--- scripts/navegacion_lateral.js
+++
@@ -1,4 +0,0 @@
-document.querySelector(".navegacion__toggle").addEventListener("click", () => {
-    const navegacion = document.querySelector(".navegacion");
-    navegacion.classList.toggle("colapsada");
-});
